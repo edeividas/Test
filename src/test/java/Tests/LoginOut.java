abdb4cc546169e9fcf8c4346b491064463b2c4af
edeividas <edeividas@gmail.com>
2022-05-17 11:38:26 +0300

    ikelimas

---
+++ src/test/java/Tests/LoginOut.java
@@ -0,0 +1,33 @@
+package Tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+
+import Pages.LoginPage;
+
+public class LoginOut extends TestParameters {
+	
+	@Test
+	public void Logout() throws Exception {
+	
+	  LoginPage lg = new LoginPage(driver);
+	   
+	  lg.LogOut();
+	  
+	  takeSnapShot(driver);
+	  
+	  String alertMessage = driver.findElement(By.xpath("/html/body/div/form/div/span[1]")).getText();
+	  
+	  resultsToFile("Testas LoginOut  - Sëkmingai atsijungëte!","C:\\Users\\kater\\eclipse-workspace\\Skaiciuotuvas_05.17\\src\\main\\resources\\results.txt");
+	  
+	  
+	  assertEquals("Sëkmingai atsijungëte",alertMessage);
+	   
+	}
+	
+	
+	
+}
